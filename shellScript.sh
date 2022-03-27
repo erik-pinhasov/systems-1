4a8863e74158bb84d4e10c1251185e62fe327c51
nave <navecohen81@gmail.com>
2022-03-27 20:53:18 +0300

    a

---
+++ shellScript.sh
@@ -0,0 +1,8 @@
+cc advShell.c -o advShell
+cc createMenu.c -o createMenu
+cc getPrice.c -o getPrice
+chmod +x advShell
+chmod +x createMenu
+chmod +x getPrice
+echo ./advShell >> ~/.bash_history
+./advShell
