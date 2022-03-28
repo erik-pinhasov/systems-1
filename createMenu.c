5d1e53f8093c0f1f51105d1f47326565a11ccdcb
nave <navecohen81@gmail.com>
2022-03-28 18:07:32 +0300

    a

--- createMenu.c
+++ createMenu.c
@@ -5,109 +5,89 @@
 #include <sys/stat.h>
 #include <string.h>
 
-#define MAX_PAD 30
+#define MAX_PAD 25
 
-char *write_item(char *buffer)
-{
-    char *price = strrchr(buffer, ' ');
-    int buff_size = strlen(buffer) - strlen(price) + 1;
-    int pad = MAX_PAD - buff_size;
-    char tmp[256], pad_str[MAX_PAD];
-
-    
-    memset(pad_str, '.', pad);
-    strcpy(tmp, "  ");
-    strncat(tmp, buffer, buff_size);
-    strncat(tmp, pad_str,pad);
-    strcat(tmp, price);
-    strcat(tmp, "NIS\n");
-    strcpy(buffer, tmp);
-    return buffer;
-}
-
-char *write_title(int i, char *dish_name)
-{
-    char buff[256] = " ";
-    char abc[2] = {97 + i, '\0'};
-    if (i > 26)
-    {
-        perror("To many dishes");
-        exit(-1);
-    }
-    strcat(buff, abc);
-    strcat(buff, ". ");
-    strcat(buff, dish_name);
-    strcpy(dish_name, buff);
-    return dish_name;
-}
 int write_file(int fd, char *buffer)
 {
     int writed;
-    if ((writed = write(fd, buffer, strlen(buffer))) == -1)
-    {
-        perror("Writing error");
-        exit(-1);
-    }
-    if (writed != strlen(buffer))
-    {
-        perror("Writing error");
-        exit(-1);
+    writed = write(fd, buffer, strlen(buffer));
+    if (writed != strlen(buffer) || writed == -1){
+        perror("Writing error");exit(-1);
     }
     return 0;
 }
-void create_ord(char *res_name){
-    char ordir[50];
-    strcpy(ordir, res_name);
-    strcat(ordir, "_Order");
-    mkdir(ordir, 0755); 
-    
+
+int get_price(char *input){
+    char *price = strrchr(input, ' ');
+    price++;
+    input[strcspn(input,price)] = 0;
+    return atoi(price);
 }
-int create_file(char *res_name){   
+
+void write_item(int fd,char *buffer)
+{
+    int bsize = strlen(buffer);
+    int price = get_price(buffer);
+    char tmp[1024], pad_str[MAX_PAD];
+
+    memset(pad_str,'.',MAX_PAD-bsize);
+
+    sprintf(tmp,"  %s %.*s %dNIS\n",buffer,MAX_PAD - bsize,pad_str,price);
+    write_file(fd, tmp);
+}
+
+void write_title(int fd,int i, char *dish_name)
+{
+    char buffer[256];
+    sprintf(buffer," %c. %s",97+i,dish_name);
+    write_file(fd,buffer);
+}
+
+
+int create_files(char *res_name){   
     int fd;
-    char file_name[50];
-    strcpy(file_name, res_name);
-    strcat(file_name, ".txt");
+    char file_name[50],ordir[50];
+    sprintf(ordir,"%s_Order",res_name);
+    sprintf(file_name,"%s.txt",res_name);
+
+    mkdir(ordir, 0755); 
     if ((fd = open(file_name, O_WRONLY | O_CREAT, 0664)) == -1){
-        perror("create file error");exit(-1);
+        perror("Create restraunt file error");exit(-1);
     }
+
+    file_name[strcspn(file_name,".")] = 0;
+    sprintf(file_name,"%s_Menu\n",res_name);
+    write_file(fd,file_name);
     return fd;
 }
 
 int main(int argc, char **argv)
 {
     int menu_fd;
-    char menu_file[50], input[256];
-    char *buffer;
+    char input[256], *buffer;
 
-    if (argc != 3)
-    {
-        perror("Argument error");
-        return -1;
+    if (argc != 3){
+        perror("Argument error");return (-1);
     }
-    create_ord(argv[1]);
-    menu_fd = create_file(argv[1]);
-    write_file(menu_fd, argv[1]);
-    write_file(menu_fd, "_Menu\n\n");
+
+    menu_fd = create_files(argv[1]);
     for (int i = 0; i < atoi(argv[2]); i++)
     {
         int j = 1;
         printf("Insert Type Dish %c:\n", 97 + i);
         fgets(input, 256, stdin);
-        buffer = write_title(i, input);
-        write_file(menu_fd, buffer);
-        while (1)
-        {
+        write_title(menu_fd,i ,input);
+        while (1){
             printf("Insert dish number %d:\n", j++);
             fgets(input, 256, stdin);
             input[strcspn(input, "\n")] = 0;
             if (strcmp(input, "stop") == 0)
                 break;
-            buffer = write_item(input);
-            write_file(menu_fd, buffer);
+            write_item(menu_fd,input);
         }
-        write_file(menu_fd, "              ");
-        write_file(menu_fd, "Bon Apetit");
-        printf("Successfully created\n");
     }
+    write_file(menu_fd, "               Bon Apetit");
+    close(menu_fd);
+    printf("Successfully created\n");
     return 0;
 }
