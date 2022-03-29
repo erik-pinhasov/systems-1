bfe80082ce243a86393fb7c55ce649aa8aa4d835
Nave Cohen <navecohen81@gmail.com>
2022-03-29 13:59:59 +0300

    a

--- shell.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p  ubin
-cc advShell.c -o ubin/advShell
-cc createMenu.c -o ubin/createMenu
-cc getPrice.c -o ubin/getPrice
-chmod +x ubin/advShell
-chmod +x ubin/createMenu
-chmod +x ubin/getPrice
-echo "./ubin/advShell" >> ~/.bash_history
-./ubin/advShell
