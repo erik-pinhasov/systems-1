ba5c275985cc45cd126ae0c997865ee1670de070
Erik Pinhasov <erik944@gmail.com>
2022-03-30 05:03:31 +0300

    a

--- shellScript.sh
+++ shellScript.sh
@@ -2,10 +2,10 @@ cc advShell.c -o advShell
 cc createMenu.c -o createMenu
 cc getPrice.c -o getPrice
 cc numOrder.c -o numOrder
-cc makeorder.c -o makeorder
+cc makeOrder.c -o makeOrder
 chmod +x advShell
 chmod +x createMenu
 chmod +x getPrice
-chmod +x makeorder
+chmod +x makeOrder
 chmod +x numOrder
 ./advShell
